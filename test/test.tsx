38da94d5dcccc103d49393efbf73f8f3c580ad0c
canlooks <364021661@qq.com>
2025-12-08 10:39:21 +0800

    1

--- test/test.tsx
+++ test/test.tsx
@@ -1,25 +1,19 @@
 import {createRoot} from 'react-dom/client'
 import {css, Global} from '@emotion/react'
-import {App, Bubble, Button, Deferred, Icon, LoadingIndicator} from '../src'
+import {App, Bubble, Button, Card, Deferred, Icon, imagePreset, LoadingIndicator, Placeholder} from '../src'
 import React, {ReactNode, useDeferredValue, useEffect, useMemo, useState} from 'react'
 import {RC, useReactive} from '@canlooks/reactive/react'
 import {faInfoCircle} from '@fortawesome/free-solid-svg-icons'
 
 const Root = RC(() => {
     return (
-        <>
-            <h1>123</h1>
-            <Bubble content="123">
-                <Button variant="plain">
-                    <Icon icon={faInfoCircle}/>
-                </Button>
-            </Bubble>
-            <Bubble content="123">
-                <Button>
-                    <Icon icon={faInfoCircle}/>
-                </Button>
-            </Bubble>
-        </>
+        <Card>
+            <Placeholder status="warning"/>
+            <Placeholder status="success"/>
+            <Placeholder status="error"/>
+            <Placeholder status="searching"/>
+            <Placeholder status="unknown" title="404"/>
+        </Card>
     )
 })
 
