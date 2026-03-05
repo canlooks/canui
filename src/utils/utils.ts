fbf17a25d0be5ec20c697abc877887de3f7e3adf
C.CanLiang <364021661@qq.com>
2026-03-05 19:31:01 +0800

    1

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -5,7 +5,13 @@ import {Id, Obj} from '../types'
  * 获取UUID
  */
 export function getRandomId() {
-    return crypto.randomUUID()
+    if (typeof crypto.randomUUID === 'function') {
+        return crypto.randomUUID()
+    }
+    // polyfill for uuid v4
+    return '10000000-1000-4000-8000-100000000000'.replace(/[018]/g, c =>
+        (+c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> +c / 4).toString(16)
+    )
 }
 
 /**
